23c360f0f5828db2381d8aee9daf596cc512393a
Claudio Jr <cla.junior21@gmail.com>
2024-07-21 12:26:14 -0300

    create the training page

--- src/RouterApp.tsx
+++ src/RouterApp.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
+import Training from "./pages/Training";
 
-export default function RouterApp(){
+export default function RouterApp() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/menu" element={<Menu />} />
+                <Route path="/training" element={<Training />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Training/index.tsx
@@ -0,0 +1,11 @@
+import { CiDumbbell } from "react-icons/ci";
+
+export default function Training(){
+    return (
+        <div className="min-h-screen pt-8">
+            <h1 className="text-3xl font-bold text-laranja flex items-center justify-center gap-2"><CiDumbbell/> Treinos</h1>
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+        </div>
+    )
+}
